7a79443e298466802a759f4f5cf00d3f9d523f43
Julien LEICHER <jleicher@gmail.com>
2018-10-11 14:13:08 +0200

    Unit tests refactoring done!

--- NEXT.md
+++ NEXT.md
@@ -1,4 +1,5 @@
 Coming next
 ===
 
-- [ ] unit test refactoring! (with `sure` and `nose`)
+- [x] unit test refactoring! (with `sure` and `nose`)
+- [ ] meta in `agent.ask` and `agent.answer` to enable more presentation options when a client accept custom properties
--- tests/test_agent.py
+++ tests/test_agent.py
@@ -1,305 +1,281 @@
 from sure import expect
-import unittest, logging
+import logging
 from unittest.mock import MagicMock
 from pytlas import Agent
 from pytlas.agent import STATE_ASK, STATE_CANCEL, STATE_ASLEEP, STATE_FALLBACK
 from pytlas.card import Card
 from pytlas.interpreters import Interpreter, Intent, SlotValue
 
-def wrapMockCall(mock_handler):
-  """Wraps the magic mock given in a pytlas handler which call the agent.done method
-  after calling the mock_handler with the raw Request object.
-  
+last_request = None
+
+def on_cancel(r):
+  """greet handler which call agent.answer with 'Cancelled'.
   """
 
-  def handler(request):
-    mock_handler(request)
+  global last_request
+  last_request = r
 
-    return request.agent.done()
+  r.agent.answer('Cancelled')
 
-  return handler
+  return r.agent.done()
 
-class TestParseIntent:
+def on_fallback(r):
+  """on_fallback handler which call agent.answer with 'Searching for <text slot value>'.
+  """
+  
+  global last_request
+  last_request = r
+
+  r.agent.answer('Searching for ' + r.intent.slot('text').first().value)
+
+  return r.agent.done()
+
+def on_block(r):
+  """block handler which does not call agent.done so it freeze transitions.
+  """
+
+  pass
+
+def on_greet(r):
+  """greet handler which call agent.answer with 'Hello you!'.
+  """
+
+  global last_request
+  last_request = r
+
+  r.agent.answer('Hello you!')
+
+  return r.agent.done()
+
+def on_lights_on(r):
+  """lights_on handler which call agent.answer with 'Turning lights on in <room slot value>'.
+  """
+
+  global last_request
+  last_request = r
+
+  room = r.intent.slot('room')
+
+  if not room:
+    return r.agent.ask('room', 'Which ones?', choices=['kitchen', 'living room', 'bedroom'])
+
+  r.agent.answer('Turning lights on in ' + room.first().value)
+
+  return r.agent.done()
+
+def on_get_forecast(r):
+  """get_forecast handler which requires date and city slots and answer with
+  'Looking in <city slot value> for <date slot value>'
+  """
+
+  global last_request
+  last_request = r
+
+  date = r.intent.slot('date').first().value
+  city = r.intent.slot('city').first().value
+
+  if not date:
+    return r.agent.ask('date', 'When?')
+
+  if not city:
+    return r.agent.ask('city', 'Where?')
+
+  r.agent.answer('Looking in %s for %s' % (city, date), cards=[Card(city, date)])
+
+  return r.agent.done()
+
+class TestAgent:
+  """For those tests, the interpreter.parse is always mocked because there is no interpreter
+  such as snips. It only tests transitions and state management.
+  """
 
   def setup(self):
     self.on_answer = MagicMock()
     self.on_ask = MagicMock()
     self.on_done = MagicMock()
 
-    self.greet_handler = MagicMock()
-    self.forecast_handler = MagicMock()
-    self.fallback_handler = MagicMock()
-    self.cancel_handler = MagicMock()
-
     self.handlers = {
-      'greet': wrapMockCall(self.greet_handler),
-      'get_forecast': wrapMockCall(self.forecast_handler),
-      STATE_CANCEL: wrapMockCall(self.cancel_handler),
-      STATE_FALLBACK: wrapMockCall(self.fallback_handler),
+      'greet': on_greet,
+      'get_forecast': on_get_forecast,
+      'lights_on': on_lights_on,
+      'block': on_block,
+      STATE_CANCEL: on_cancel,
+      STATE_FALLBACK: on_fallback,
     }
     self.interpreter = Interpreter('test', 'en')
-    self.interpreter.intents = list(self.handlers.keys())
+    self.interpreter.intents = list(self.handlers.keys()) + ['intent_without_handler']
     self.agent = Agent(self.interpreter, 
       handlers=self.handlers, 
       on_answer=self.on_answer,
       on_ask=self.on_ask,
       on_done=self.on_done)
 
-  def test_it_should_parse_string_as_intent(self):
+  def test_it_should_queue_string_as_intent(self):
     self.agent.queue_intent('greet')
 
-    self.greet_handler.assert_called_once()
+    expect(last_request.intent.name).to.equal('greet')
+    self.on_answer.assert_called_once_with('Hello you!', None)
+    self.on_done.assert_called_once()
+    expect(self.agent.state).to.equal(STATE_ASLEEP)
 
-  def test_it_should_parse_string_and_kwargs_as_intent_with_slot_values(self):
+  def test_it_should_queue_string_and_kwargs_as_intent_with_slot_values(self):
     self.agent.queue_intent('greet', name='Julien')
 
-    self.greet_handler.assert_called_once()
+    self.on_answer.assert_called_once()
+    self.on_done.assert_called_once()
     
-    arg = self.greet_handler.call_args[0][0].intent
-
-    expect(arg).to.be.an(Intent)
-    expect(arg.name).to.equal('greet')
-    expect(arg.slots).to.have.length_of(1)
-    expect(arg.slot('name').first().value).to.equal('Julien')
+    expect(last_request.intent.name).to.equal('greet')
+    expect(last_request.intent.slots).to.have.length_of(1)
+    expect(last_request.intent.slot('name').first().value).to.equal('Julien')
+    expect(self.agent.state).to.equal(STATE_ASLEEP)
 
   def test_it_should_trigger_intent(self):
     self.agent.queue_intent(Intent('greet'))
 
-    self.greet_handler.assert_called_once()
+    expect(last_request.intent.name).to.equal('greet')
+    self.on_answer.assert_called_once_with('Hello you!', None)
+    self.on_done.assert_called_once()
+    expect(self.agent.state).to.equal(STATE_ASLEEP)
 
-class AgentTests(unittest.TestCase):
+  def test_it_should_handle_simple_intent(self):
+    self.interpreter.parse = MagicMock(return_value=[Intent('greet')])
 
-  def test_simple_intent(self):
-    interp = Interpreter('test', 'en')
-    handlers = {
-      'lights_on': MagicMock(),
-      'lights_off': MagicMock(),
-    }
-    interp.intents = list(handlers.keys())
-    interp.parse = MagicMock(return_value=[
-      Intent('lights_on')
+    self.agent.parse('hello')
+
+    self.on_answer.assert_called_once_with('Hello you!', None)
+    self.on_done.assert_called_once()
+    expect(self.agent.state).to.equal(STATE_ASLEEP)
+
+  def test_it_should_have_cards(self):
+    self.interpreter.parse = MagicMock(return_value=[Intent('get_forecast', date='tomorrow', city='rouen')])
+
+    self.agent.parse('will it rain in rouen tomorrow')
+
+    self.on_answer.assert_called_once()
+
+    call_args = self.on_answer.call_args[0]
+    expect(call_args).to.have.length_of(2)
+    expect(call_args[0]).to.equal('Looking in rouen for tomorrow')
+    expect(call_args[1]).to.be.a(list)
+    expect(call_args[1]).to.have.length_of(1)
+    expect(call_args[1][0].header).to.equal('rouen')
+    expect(call_args[1][0].text).to.equal('tomorrow')
+
+  def test_it_should_handle_multiple_intents(self):
+    self.interpreter.parse = MagicMock(return_value=[
+      Intent('greet'), 
+      Intent('block'), 
+      Intent('lights_on', room='kitchen'),
     ])
 
-    agt = Agent(interp, handlers)
-    agt.parse('turn lights on')
+    self.agent.parse('hello, can you turn the lights on in kitchen')
 
-    handlers['lights_on'].assert_called_once()
-    handlers['lights_off'].assert_not_called()
+    self.on_answer.assert_called_once_with('Hello you!', None)
+    self.on_done.assert_called_once()
 
-  def test_multiple_intents(self):
+    greet_id = last_request.id
 
-    lights_on_request = None
-    lights_off_request = None
+    expect(self.agent.state).to.equal('block')
 
-    def lights_on(r):
-      nonlocal lights_on_request
-      lights_on_request = r
+    # Since the block intent does not call it to test it more easily, call the agent.done now to process the next intent
+    self.agent.done()
 
-    def lights_off(r):
-      nonlocal lights_off_request
-      lights_off_request = r
+    self.on_answer.assert_called_with('Turning lights on in kitchen', None)
 
-    interp = Interpreter('test', 'en')
-    handlers = {
-      'lights_on': lights_on,
-      'lights_off': lights_off,
-    }
-    interp.intents = list(handlers.keys())
-    interp.parse = MagicMock(return_value=[
-      Intent('lights_on'),
-      Intent('lights_off')
-    ])
-    
-    agt = Agent(interp, handlers)
-    agt.parse('turn lights on and off', meta='a meta value')
+    expect(self.on_done.call_count).to.equal(3)
+    expect(last_request.id).to_not.equal(greet_id)
+    expect(self.agent.state).to.equal(STATE_ASLEEP)
 
-    self.assertIsNotNone(lights_on_request)
-    self.assertEqual({ 'meta': 'a meta value' }, lights_on_request.intent.meta)
-    self.assertIsNone(lights_off_request)
-    self.assertEqual('lights_on', agt.state)
+  def test_it_should_add_parse_meta_to_intent_meta(self):
+    greet_intent = Intent('greet')
+    lights_intent = Intent('lights_on', room='kitchen')
 
-    agt.done() # Since we use a mock, we should call it ourselves
-
-    self.assertIsNotNone(lights_off_request)
-    self.assertEqual({ 'meta': 'a meta value' }, lights_off_request.intent.meta)
-    self.assertEqual('lights_off', agt.state)
-
-    self.assertNotEqual(lights_on_request.id, lights_off_request.id)
-
-  def test_ask(self):
-    
-    request = None
-
-    def handler(r):
-      nonlocal request
-      request = r
-      r.agent.ask('a_slot', 'Please fill the slot')
-
-    interp = Interpreter('test', 'en')
-    ask = MagicMock()
-    done = MagicMock()
-    
-    handlers = {
-      'should_ask': handler,
-    }
-    interp.intents = list(handlers.keys())
-    interp.parse = MagicMock(return_value=[
-      Intent('should_ask'),
-    ])
-    interp.parse_slot = MagicMock(return_value=[
-      SlotValue('a value'),
+    self.interpreter.parse = MagicMock(return_value=[
+      greet_intent,
+      lights_intent,
     ])
 
-    agt = Agent(interp, handlers, on_ask=ask, on_done=done)
-    agt.parse('You should ask for a slot')
+    self.agent.parse('hello, can you turn the lights on in kitchen', latitude=49.44, longitude=1.09)
 
-    ask.assert_called_once_with('a_slot', 'Please fill the slot', None)
-    done.assert_called_once()
-    self.assertEqual(STATE_ASK, agt.state)
+    expect(greet_intent.meta).to.equal({ 'latitude': 49.44, 'longitude': 1.09 })
+    expect(lights_intent.meta).to.equal({ 'latitude': 49.44, 'longitude': 1.09 })
+    expect(last_request.intent.meta).to.equal({ 'latitude': 49.44, 'longitude': 1.09 })
+  
+  def test_it_should_do_nothing_and_returns_to_the_asleep_state_when_no_handler_was_found(self):
+    self.interpreter.parse = MagicMock(return_value=[Intent('intent_without_handler')])
 
-    assertion_success = False
+    self.agent.parse('an intent without a handler')
 
-    def final_handler(r):
-      nonlocal request, assertion_success
+    self.on_done.assert_called_once()
+    expect(self.agent.state).to.equal(STATE_ASLEEP)
 
-      assertion_success = r.id == request.id and r.intent.slot('a_slot').first().value == 'a value' and r.intent.meta == { 'meta': 'a meta value' }
+  def test_it_should_be_in_the_ask_state_when_a_skill_ask_for_slot(self):
+    self.interpreter.parse = MagicMock(return_value=[Intent('get_forecast')])
 
-    handlers['should_ask'] = final_handler
+    self.agent.parse('will it be sunny')
 
-    agt.parse('Parse slot should be called', meta='a meta value')
-    interp.parse_slot.assert_called_once_with('should_ask', 'a_slot', 'Parse slot should be called')
+    self.on_ask.assert_called_once_with('date', 'When?', None)
+    self.on_done.assert_called_once()
+    self.on_answer.assert_not_called()
+    expect(self.agent.state).to.equal(STATE_ASK)
 
-    self.assertTrue(assertion_success)
+    self.agent.parse('tomorrow')
 
-  def test_ask_with_choices(self):
+    self.on_ask.assert_any_call('city', 'Where?', None)
+    expect(self.on_done.call_count).to.equal(2)
+    self.on_answer.assert_not_called()
+    expect(self.agent.state).to.equal(STATE_ASK)
 
-    assertion_success = False
-    choices = ['kitchen', 'living room', 'bedroom']
+    self.agent.parse('rouen')
 
-    def handler(r):
-      nonlocal assertion_success
+    self.on_answer.assert_called_once()
+    expect(self.on_answer.call_args[0][0]).to.equal('Looking in rouen for tomorrow')
+    expect(self.agent.state).to.equal(STATE_ASLEEP)
+    expect(self.on_done.call_count).to.equal(3)
 
-      rooms = r.intent.slot('rooms')
+  def test_it_should_match_on_choices_when_asking_with_choices(self):
+    self.interpreter.parse = MagicMock(return_value=[Intent('lights_on')])
 
-      if not rooms:
-        return r.agent.ask('rooms', 'Choose a room in one of those', choices)
+    self.agent.parse('turn the lights on')
 
-      assertion_success = rooms.first().value == 'living room'
+    self.on_ask.assert_called_once_with('room', 'Which ones?', ['kitchen', 'living room', 'bedroom'])
+    expect(self.agent.state).to.equal(STATE_ASK)
 
-      return r.agent.done()
-    
-    ask = MagicMock()
-    handlers = {
-      'lights_on': handler,
-    }
-    interp = Interpreter('test', 'en')
-    interp.intents = list(handlers.keys())
-    interp.parse = MagicMock(return_value=[
-      Intent('lights_on'),
-    ])
+    self.agent.parse('in the living room')
 
-    agt = Agent(interp, handlers, on_ask=ask)
-    agt.parse('turn the lights on')
+    self.on_answer.assert_called_with('Turning lights on in living room', None)
+    expect(self.on_done.call_count).to.equal(2)
 
-    ask.assert_called_once_with('rooms', 'Choose a room in one of those', choices)
-    self.assertEqual(STATE_ASK, agt.state)
-    self.assertEqual(choices, agt._choices)
+  def test_it_should_cancel_intent_when_cancel_is_caught(self):
+    self.interpreter.parse = MagicMock(return_value=[Intent('lights_on')])
 
-    agt.parse('in the living room')
+    self.agent.parse('turn the lights on')
 
-    self.assertTrue(assertion_success)
+    expect(self.agent.state).to.equal(STATE_ASK)
 
-  def test_cancel(self):
-    handlers = {
-      'lights_on': lambda r: r.agent.ask('rooms', 'Please specify a room'),
-    }
-    interp = Interpreter('test', 'en')
-    interp.intents = list(handlers.keys())
-    interp.parse = MagicMock(return_value=[
-      Intent('lights_on'),
-    ])
+    self.interpreter.parse = MagicMock(return_value=[Intent(STATE_CANCEL)])
 
-    agt = Agent(interp, handlers)
-    agt.parse('turn lights on')
+    self.agent.parse('cancel')
 
-    self.assertEqual(STATE_ASK, agt.state)
+    self.on_answer.assert_called_once_with('Cancelled', None)
+    expect(self.agent.state).to.equal(STATE_ASLEEP)
 
-    interp.parse = MagicMock(return_value=[
-      Intent(STATE_CANCEL),
-    ])
+  def test_it_should_call_the_fallback_intent_when_no_matching_intent_could_be_found(self):
+    self.agent.parse('should go in fallback')
 
-    agt.parse('cancel')
+    self.on_answer.assert_called_once_with('Searching for should go in fallback', None)
+    self.on_done.assert_called_once()
+    expect(self.agent.state).to.equal(STATE_ASLEEP)
+    expect(last_request.intent.name).to.equal(STATE_FALLBACK)
+    expect(last_request.intent.slots).to.have.length_of(1)
+    expect(last_request.intent.slot('text').first().value).to.equal('should go in fallback')
 
-    self.assertEqual(STATE_ASLEEP, agt.state)
-    self.assertIsNone(agt._choices)
-    self.assertIsNone(agt._request)
-    self.assertIsNone(agt._asked_slot)
+  def test_it_should_handle_interpreter_changes_with_build(self):
+    expect(self.agent._machine.states).to_not.contain('something_else')
 
-  def test_build(self):
-    interp = Interpreter('test', 'en')
-    interp.intents = ['intent_one', 'intent_two']
-    interp.parse = MagicMock(return_value=[
-      Intent('intent_three'),
-    ])
-    handler = MagicMock()
-    done = MagicMock()
-    agt = Agent(interp, { 'intent_three': handler }, on_done=done)
+    self.interpreter.intents.append('something_else')
 
-    self.assertIsNotNone(agt._machine)
-    self.assertEqual(6, len(agt._machine.states))
+    self.agent.build()
 
-    agt.parse('something')
+    expect(self.agent._machine.states).to.contain('something_else')
 
-    handler.assert_not_called()
-
-    interp.intents = ['intent_one', 'intent_two', 'intent_three']
-    self.assertEqual(6, len(agt._machine.states))
-
-    agt.build()
-
-    self.assertEqual(7, len(agt._machine.states))
-    done.assert_called_once()
-
-    agt.parse('something')
-
-    handler.assert_called_once()
-
-  def test_fallback(self):
-
-    assertion_success = False
-
-    def handler(r):
-      nonlocal assertion_success
-
-      assertion_success = r.intent.slot('text').first().value == 'something not catched'
-
-    handlers = {
-      STATE_FALLBACK: handler,
-    }
-    interp = Interpreter('test', 'en')
-    interp.intents = list(handlers.keys())
-    interp.parse = MagicMock(return_value=[])
-
-    agt = Agent(interp, handlers)
-    agt.parse('something not catched')
-
-    self.assertTrue(assertion_success)
-
-  def test_cards(self):
-    card = Card('Title', 'Content text')
-    
-    def handler(r):
-      nonlocal card
-      r.agent.answer('Some text', card)
-
-      return r.agent.done()
-
-    answer = MagicMock()
-    handlers = {
-      STATE_FALLBACK: handler,
-    }
-    interp = Interpreter('test', 'en')
-    agt = Agent(interp, handlers, on_answer=answer)
-    agt.parse('something not matched')
-
-    answer.assert_called_once_with('Some text', [card])
