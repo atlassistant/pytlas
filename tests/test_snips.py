f81fc5c63c53b3d8b5d99cf754b089c77abdf9cd
Julien LEICHER <jleicher@gmail.com>
2018-10-11 15:22:31 +0200

    Now snips tests use a generator to test against a cached interpreter too

--- tests/test_snips.py
+++ tests/test_snips.py
@@ -6,8 +6,15 @@ try:
   from pytlas.interpreters.snips import SnipsInterpreter
   
   # Train the interpreter once to speed up tests
-  interpreter = SnipsInterpreter('en')
-  interpreter.fit_from_file(os.path.join(os.path.dirname(__file__), 'training.json'))
+  fitted_interpreter = SnipsInterpreter('en')
+  fitted_interpreter.fit_from_file(os.path.join(os.path.dirname(__file__), 'training.json'))
+
+  cached_interpreter = SnipsInterpreter('en', os.path.join(os.path.dirname(__file__), 'snips_interpreter_cache'))
+  cached_interpreter.load_from_cache()
+
+  # Each test method will be run on a freshly fitted engine and on a cached one to be sure
+  # there is no particular cases
+  interpreters = [fitted_interpreter, cached_interpreter]
 
   class TestSnipsInterpreter:
 
@@ -18,11 +25,18 @@ try:
       expect(i.parse('a message')).to.be.empty
       expect(i.parse_slot('get_forecast', 'date', 'tomorrow')).to.be.empty
 
-    def test_it_should_contains_intents_defined_in_the_dataset(self):
+    def it_should_contains_intents_defined_in_the_dataset(self, interpreter):
       expect(interpreter.is_ready).to.be.true
-      expect(interpreter.intents).to.equal(['lights_on', 'lights_off', 'get_forecast'])
+      expect(interpreter.intents).to.have.length_of(3)
+      expect(interpreter.intents).to.contain('lights_on')
+      expect(interpreter.intents).to.contain('lights_off')
+      expect(interpreter.intents).to.contain('get_forecast')
 
-    def test_it_should_parse_an_intent_with_slots_correctly(self):
+    def test_it_should_contains_intents_defined_in_the_dataset(self):
+      for interpreter in interpreters:
+        yield self.it_should_contains_intents_defined_in_the_dataset, interpreter
+
+    def it_should_parse_an_intent_with_slots_correctly(self, interpreter):
       intents = interpreter.parse('will it rain in Paris and London today')
 
       expect(intents).to.be.a(list)
@@ -47,8 +61,12 @@ try:
       expect(locations).to.contain('paris')
       expect(locations).to.contain('london')
 
-    def test_it_should_parse_intent_with_date_range_correctly(self):
-      intents = interpreter.parse('will it rain between from the third of september 2018 to the fifth of september 2018')
+    def test_it_should_parse_an_intent_with_slots_correctly(self):
+      for interpreter in interpreters:
+        yield self.it_should_parse_an_intent_with_slots_correctly, interpreter
+
+    def it_should_parse_intent_with_date_range_correctly(self, interpreter):
+      intents = interpreter.parse('will it rain from the third of september 2018 to the fifth of september 2018')
 
       expect(intents).to.have.length_of(1)
 
@@ -65,7 +83,11 @@ try:
       expect(slot.meta['value']['from'].startswith('2018-09-03 00:00:00')).to.be.true
       expect(slot.meta['value']['to'].startswith('2018-09-06 00:00:00')).to.be.true
     
-    def test_it_should_parse_builtin_slot_correctly(self):
+    def test_it_should_parse_intent_with_date_range_correctly(self):
+      for interpreter in interpreters:
+        yield self.it_should_parse_intent_with_date_range_correctly, interpreter
+
+    def it_should_parse_builtin_slot_correctly(self, interpreter):
       slots = interpreter.parse_slot('get_forecast', 'date', 'today')
 
       expect(slots).to.have.length_of(1)
@@ -75,7 +97,11 @@ try:
       expect(slot.value).to.contain(datetime.datetime.now().date().isoformat())
       expect(slot.meta['value']['kind']).to.equal('InstantTime')
 
-    def test_it_should_parse_builtin_slot_range_correctly(self):
+    def test_it_should_parse_builtin_slot_correctly(self):
+      for interpreter in interpreters:
+        yield self.it_should_parse_builtin_slot_correctly, interpreter
+
+    def it_should_parse_builtin_slot_range_correctly(self, interpreter):
       slots = interpreter.parse_slot('get_forecast', 'date', 'from the third of september 2018 to the fifth of september 2018')
 
       expect(slots).to.have.length_of(1)
@@ -87,29 +113,49 @@ try:
       expect(slot.meta['value']['from'].startswith('2018-09-03 00:00:00')).to.be.true
       expect(slot.meta['value']['to'].startswith('2018-09-06 00:00:00')).to.be.true
 
-    def test_it_should_parse_custom_extensible_slot_correctly(self):
+    def test_it_should_parse_builtin_slot_range_correctly(self):
+      for interpreter in interpreters:
+        yield self.it_should_parse_builtin_slot_range_correctly, interpreter
+
+    def it_should_parse_custom_extensible_slot_correctly(self, interpreter):
       slots = interpreter.parse_slot('get_forecast', 'location', 'Strasbourg')
 
       expect(slots).to.have.length_of(1)
       expect(slots[0].value).to.equal('Strasbourg')
 
-    def test_it_should_parse_custom_slot_correctly(self):
+    def test_it_should_parse_custom_extensible_slot_correctly(self):
+      for interpreter in interpreters:
+        yield self.it_should_parse_custom_extensible_slot_correctly, interpreter
+
+    def it_should_parse_custom_slot_correctly(self, interpreter):
       slots = [s.value for s in interpreter.parse_slot('lights_on', 'room', 'kitchen and bedroom')]
 
       expect(slots).to.have.length_of(2)
       expect(slots).to.equal(['kitchen', 'bedroom'])
 
-    def test_it_should_parse_custom_slot_with_synonyms_correctly(self):
+    def test_it_should_parse_custom_slot_correctly(self):
+      for interpreter in interpreters:
+        yield self.it_should_parse_custom_slot_correctly, interpreter
+
+    def it_should_parse_custom_slot_with_synonyms_correctly(self, interpreter):
       slots = [s.value for s in interpreter.parse_slot('lights_on', 'room', 'kitchen and cellar please')]
 
       expect(slots).to.have.length_of(2)
       expect(slots).to.equal(['kitchen', 'basement'])
 
-    def test_it_should_parse_unknown_slot_correctly(self):
+    def test_it_should_parse_custom_slot_with_synonyms_correctly(self):
+      for interpreter in interpreters:
+        yield self.it_should_parse_custom_slot_with_synonyms_correctly, interpreter
+
+    def it_should_parse_unknown_slot_correctly(self, interpreter):
       slots = interpreter.parse_slot('get_forecast', 'something', 'given value expected')
 
       expect(slots).to.have.length_of(1)
       expect(slots[0].value).to.equal('given value expected')
+
+    def test_it_should_parse_unknown_slot_correctly(self):
+      for interpreter in interpreters:
+        yield self.it_should_parse_unknown_slot_correctly, interpreter
 
 except ImportError:
   print ('snips_nlu could not be imported, tests will be skipped')
