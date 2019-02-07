ebc97b8e2f87dc698b259ca950a9e07fb9d363fd
Julien Leicher <jleicher@gmail.com>
2019-02-07 11:49:40 +0100

    Added hooks to the pytlas __init__

--- pytlas/__init__.py
+++ pytlas/__init__.py
@@ -4,4 +4,4 @@ from pytlas.card import Card
 from pytlas.skill import intent, meta, register as register_intent, register_metadata
 from pytlas.localization import translations, register as register_translations
 from pytlas.training import training, register as register_training
-from pytlas.hooks import on, register as register_hook
+from pytlas.hooks import on, register as register_hook, ON_AGENT_CREATED, ON_AGENT_DESTROYED
