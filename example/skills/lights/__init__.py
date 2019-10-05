7de0655109bfd302314a7fd547f69633673630d4
Jean-Michel Lekston <lekston.jm@gmail.com>
2019-10-05 19:04:24 +0200

    Feat/lights skill sample (#42)
    * Lights skills sample remix!

--- example/skills/lights/__init__.py
+++ example/skills/lights/__init__.py
@@ -1,9 +1,1 @@
-from pytlas.understanding.training import GLOBAL_TRAININGS
-from pytlas.handling.localization import GLOBAL_TRANSLATIONS
-from pytlas.ioutils import read_file
 from .skill import *
-import json
-
-GLOBAL_TRANSLATIONS.register('fr', lambda: json.loads(read_file('lights.fr.json', relative_to=__file__)))
-GLOBAL_TRAININGS.register('en', lambda: read_file('lights.en.dsl', relative_to=__file__))
-GLOBAL_TRAININGS.register('fr', lambda: read_file('lights.fr.dsl', relative_to=__file__))
--- example/skills/lights/test_lights.py
+++ example/skills/lights/test_lights.py
@@ -4,29 +4,59 @@ import os
 
 agent = create_skill_agent(os.path.dirname(__file__))
 
+
 class TestLights:
 
-  def setup(self):
-    agent.model.reset()
+    def setup(self):
+        agent.model.reset()
 
-  def test_it_should_turn_lights_on(self):
-    agent.parse('Turn the lights on in kitchen please')
+    def test_it_should_turn_lights_on(self):
+        agent.parse('Turn the lights on in kitchen please')
 
-    call = agent.model.on_answer.get_call(0)
+        call = agent.model.on_answer.get_call(0)
 
-    expect(call.text).to.equal('Turning lights on in kitchen')
+        expect(call.text).to.equal('Turning lights on in kitchen')
+        expect(call.skill).to.equal('lights')
+        expect(call.action).to.equal('on')
+        expect(call.rooms).to.equal('kitchen')
 
-  def test_it_should_turn_lights_on_and_ask_for_rooms(self):
-    agent.parse('Turn the lights on')
+    def test_it_should_turn_lights_off(self):
+        agent.parse('Turn the lights off in kitchen please')
 
-    call = agent.model.on_ask.get_call()
+        call = agent.model.on_answer.get_call(0)
 
-    expect(call.slot).to.equal('room')
-    expect(call.text).to.be.within(['For which rooms?', 'Which rooms Sir?', 'Please specify some rooms'])
-    expect(call.choices).to.be.none
+        expect(call.text).to.equal('Turning lights off in kitchen')
+        expect(call.skill).to.equal('lights')
+        expect(call.action).to.equal('off')
+        expect(call.rooms).to.equal('kitchen')
 
-    agent.parse('In the kitchen please')
+    def test_it_should_turn_lights_on_and_ask_for_rooms(self):
+        agent.parse('Turn the lights on')
 
-    call = agent.model.on_answer.get_call()
+        call = agent.model.on_ask.get_call()
 
-    expect(call.text).to.equal('Turning lights on in kitchen')
+        expect(call.slot).to.equal('room')
+        expect(call.text).to.be.within(
+            ['For which rooms?', 'Which rooms Sir?', 'Please specify some rooms'])
+        expect(call.choices).to.be.none
+
+        agent.parse('In the kitchen please')
+
+        call = agent.model.on_answer.get_call()
+
+        expect(call.text).to.equal('Turning lights on in kitchen')
+
+    def test_it_should_turn_lights_on_and_ask_among_available_rooms(self):
+        agent.parse('Turn the lights on in cellar')
+
+        call = agent.model.on_ask.get_call()
+
+        expect(call.slot).to.equal('room')
+        expect(call.text).to.equal("Please choose a room  among?")
+        expect(call.choices).to.equal(['kitchen', 'bedroom'])
+
+        agent.parse('In the kitchen please')
+
+        call = agent.model.on_answer.get_call()
+
+        expect(call.text).to.equal('Turning lights on in kitchen')
